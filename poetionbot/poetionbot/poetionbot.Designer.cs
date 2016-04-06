03c515836e4fc807ec7d91be990c5da7060382ca
Xackery <xackery@gmail.com>
2016-04-06 00:54:22 -0700

    Added icon, and created systray system

--- poetionbot/poetionbot/poetionbot.Designer.cs
+++ poetionbot/poetionbot/poetionbot.Designer.cs
@@ -28,26 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(poetionbot));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.mnuAttach = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // notifyIcon1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "systrayIcon";
+            this.notifyIcon1.Visible = true;
+           // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuAttach});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
             // 
             // mnuAttach
             // 
             this.mnuAttach.Name = "mnuAttach";
-            this.mnuAttach.Size = new System.Drawing.Size(113, 20);
-            this.mnuAttach.Text = "&Attach To Process";
+            this.mnuAttach.Size = new System.Drawing.Size(152, 22);
+            this.mnuAttach.Text = "Attach Process";
             this.mnuAttach.Click += new System.EventHandler(this.mnuAttach_Click);
             // 
             // poetionbot
@@ -55,20 +62,18 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "poetionbot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "poetionbot";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.poetionbot_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem mnuAttach;
     }
 }
--- poetionbot/poetionbot/poetionbot.cs
+++ poetionbot/poetionbot/poetionbot.cs
@@ -7,20 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace poetionbot
 {
     public partial class poetionbot : Form
     {
+        
+        private IntPtr handle;
+
         public poetionbot()
         {
             InitializeComponent();
         }
-        
+
+        private void poetionbot_Load(object sender, EventArgs e)
+        {
+
+        }
 
         private void mnuAttach_Click(object sender, EventArgs e)
         {
+                        
+            if (handle != null)
+            {
+                checkHandleStatus();
+                return;
+            }
 
+            var processes = w32.GetProcessList("notepad.exe");
+            if (processes.Length == 1)
+            {
+                handle = w32.AttachProcess(processes[0]);
+                checkHandleStatus();
+            }
+        }
+
+        private void checkHandleStatus()
+        {
+            if (handle == null) {
+                mnuAttach.Text = "Attach To Path of Exile";
+            } else
+            {
+                mnuAttach.Text = "Detach From Path of Exile";
+            }
         }
     }
 }
